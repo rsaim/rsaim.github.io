4974d310e91d2de58a264acc8653e7e3a413a02f
Saim Raza <saimorsaim@gmail.com>
2025-07-05 18:34:39 -0400

    Code cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,7 @@ function Footer() {
   return (
     <Container fluid className="footer">
       <Row>
-        <Col md="4" className="footer-copywright">
-          {/* <h3>Designed and Developed by Soumyajit Behera</h3> */}
-        </Col>
+        <Col md="4" className="footer-copywright"></Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} SR</h3>
         </Col>
